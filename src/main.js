34af3ebba32c6f875f3889271ec5a648d4d2502f
QendrimG5 <38256709+QendrimG5@users.noreply.github.com>
2023-01-06 23:36:48 +0100

    Nav-header-responsive

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
+/* eslint-disable prettier/prettier */
 import './style.css';
 import './components/navbar/GHeader';
+
+
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
