ed13908f927eedc1723e878fb62086f5b132adde
QendrimG5 <38256709+QendrimG5@users.noreply.github.com>
2023-01-12 05:57:45 +0100

    adding some features

---
+++ src/helpers/horizintal-line.js
@@ -0,0 +1,13 @@
+import { html } from 'lit';
+import { LitLightElement } from '../lib/LitElement';
+
+class MyElement extends LitLightElement {
+	render() {
+		return html`
+			<section class="m-[3%]">
+				<div class="h-[2px]  w-[100%]  rounded-full bg-gray-200"></div>
+			</section>
+		`;
+	}
+}
+customElements.define('h-line', MyElement);
