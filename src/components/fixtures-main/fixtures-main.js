11e06e78dd6064f4783bf432827bf90bc349e456
QendrimG5 <38256709+QendrimG5@users.noreply.github.com>
2023-01-19 01:57:26 +0100

    Making data dynamic, adding filter methods on  games by Statuss

--- src/components/fixtures-main/fixtures-main.js
+++ src/components/fixtures-main/fixtures-main.js
@@ -1,11 +1,59 @@
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable promise/always-return */
 import { html } from 'lit';
+import moment from 'moment';
 import { LitLightElement } from '../../lib/LitElement';
 
 class FixturesMain extends LitLightElement {
+	static get properties() {
+		return {
+			data: { type: Array },
+			url: { type: Array },
+			activeClass: { type: String },
+		};
+	}
+
+	constructor() {
+		super();
+		this.data = [];
+		this.activeClass =
+			'border-b-[5px] h-full cursor-pointer border-yellow-400 text-yellow-400';
+		this.url = [
+			'https://api.npoint.io/cfdd9340ece0aa795c9e',
+			'https://api.npoint.io/832db32c2f7ed1d3b542',
+			'https://api.npoint.io/1ba04a17c40d1ca4244d',
+		];
+		this.activeHeading = 0;
+		this.fetchData(this.url[0]);
+	}
+
+	fetchData(url) {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				this.data = data.slice(10, 20);
+			})
+			.catch((error) => {
+				this.error = error.message;
+				this.loading = false;
+			});
+	}
+
+	changeUrlandActive(index) {
+		this.activeHeading = index;
+		this.fetchData(this.url[index]);
+	}
+
 	render() {
 		return html`
 			<section class="m-[3%]">
-				<div class="w-full rounded-t-[40px] bg-gray-600">
+				<div class="w-full rounded-t-[40px] ">
 					<div class="relative h-[125px] w-full border-b-[5px] border-gray-300">
 						<div class="flex">
 							<p
@@ -22,74 +70,97 @@ class FixturesMain extends LitLightElement {
 							class="absolute -bottom-[5px] flex h-[50px] w-[100%] items-start gap-[6%] md:gap-[6%]"
 						>
 							<p
-								class="h-full border-b-[5px] border-yellow-500 pl-[2.5%] text-center text-[14px] text-yellow-500 md:text-[17px]"
+								class=" h-full  pl-[2.5%] text-center text-[14px]  md:text-[17px]"
+								class="${this.activeHeading === 0
+									? this.activeClass
+									: 'text-gray-200 cursor-pointer font-medium'}"
+								@click="${() => this.changeUrlandActive(0)}"
 							>
 								Latest Match
 							</p>
 							<p
-								class="h-full text-center text-[14px] text-gray-200 md:text-[17px]"
+								class="h-full text-center text-[14px]  md:text-[17px]"
+								class="${this.activeHeading === 1
+									? this.activeClass
+									: 'text-gray-200 cursor-pointer font-medium'}"
+								@click="${() => this.changeUrlandActive(1)}"
 							>
 								Live Matches
 							</p>
 							<p
-								class="h-full text-center text-[14px] text-gray-200 md:text-[17px]"
+								class="h-full text-center text-[14px] md:text-[17px]"
+								class="${this.activeHeading === 2
+									? this.activeClass
+									: 'text-gray-200 cursor-pointer font-medium'}"
+								@click="${() => this.changeUrlandActive(2)}"
 							>
 								Coming Matches
 							</p>
 						</div>
 					</div>
 					<div
-						class="no-scrollbar mx-auto mt-[20px] h-[455px] w-[95%] overflow-y-scroll"
+						class="no-scrollbar mx-auto mt-[10px] h-[500px] w-[95%] cursor-pointer overflow-y-scroll"
 					>
-						<div
-							class="grid h-[55px] w-full items-center gap-[1%] rounded-2xl bg-slate-900 md:grid-cols-2"
-						>
-							<div class="col-span-1 h-full w-full">
-								<div class="relative flex h-full w-full items-center">
-									<img
-										class="absolute left-[2%] h-[30px] w-[30px] sm:h-[35px] sm:w-[35px] md:h-[35px] md:w-[35px]"
-										src="./images/33.png"
-										alt=""
-									/>
-									<p
-										class="absolute left-[15%] w-[25%] text-[15px] text-gray-200 sm:left-[12%] sm:text-[17px] md:left-[15%] md:text-[17px] lg:left-[15%] xl:left-[10%] xl:text-[17px]"
-									>
-										Manchester United
-									</p>
-									<p
-										class="absolute left-[42.5%] w-[15%] rounded-[20px] bg-slate-500 py-[5px] px-[10px] text-center text-lime-400 sm:text-[18px]"
-									>
-										1-2
-									</p>
-									<p
-										class="xl:rigth-[10%] absolute right-[13%] w-[25%] text-[15px] text-gray-200 sm:right-[10%] sm:text-[17px] md:right-[15%] md:text-[17px] lg:right-[10%] xl:text-[17px]"
-									>
-										Manchester United
-									</p>
-									<img
-										class="absolute right-[2%] h-[30px] w-[30px] sm:h-[35px] sm:w-[35px] md:h-[35px] md:w-[35px]"
-										src="./images/33.png"
-										alt=""
-									/>
-								</div>
-							</div>
+						${this.data.map(
+							(item, index) => html`
+								<div
+									class="${index % 2 === 0
+										? 'bg-slate-900'
+										: 'bg-gray-700'} mt-[0.5%] grid h-[55px] w-full items-center gap-[1%] rounded-xl md:grid-cols-2"
+								>
+									<div class="col-span-1 h-full w-full">
+										<div class="relative flex h-full w-full items-center">
+											<img
+												class="absolute left-[2%] h-[30px] w-[30px] sm:h-[35px] sm:w-[35px] md:h-[35px] md:w-[35px]"
+												src="${item.teams.home.logo}"
+												alt=""
+											/>
+											<p
+												class="absolute left-[15%] w-[25%] text-[15px] text-gray-200 sm:left-[12%] sm:text-[17px] md:left-[15%] md:text-[17px] lg:left-[15%] xl:left-[10%] xl:text-[17px]"
+											>
+												${item.teams.home.name}
+											</p>
+											<p
+												class="absolute left-[42.5%] w-[15%] rounded-[20px] bg-slate-500 py-[5px] px-[10px] text-center text-lime-400 sm:text-[18px]"
+											>
+												${item.goals.home}-${item.goals.away}
+											</p>
+											<p
+												class="xl:rigth-[10%] absolute right-[13%] w-[25%] text-[15px] text-gray-200 sm:right-[10%] sm:text-[17px] md:right-[15%] md:text-[17px] lg:right-[10%] xl:text-[17px]"
+											>
+												${item.teams.away.name}
+											</p>
+											<img
+												class="absolute right-[2%] h-[30px] w-[30px] sm:h-[35px] sm:w-[35px] md:h-[35px] md:w-[35px]"
+												src="${item.teams.away.logo}"
+												alt=""
+											/>
+										</div>
+									</div>
 
-							<div class="relative col-span-1 hidden h-full w-full md:grid">
-								<div class="full flex h-full items-center">
-									<p
-										class="ml-[10%] flex w-[20%] items-center justify-center rounded-[10px] bg-red-200 px-[10px] py-[5px] text-red-500"
-									>
-										FT
-									</p>
-									<p class="ml-[20%] w-[20%] text-gray-200">19.03.2023</p>
-									<p class="absolute right-[5%]">
-										<i
-											class="fa-solid fa-circle-info text-[25px] text-gray-200"
-										></i>
-									</p>
+									<div class="relative col-span-1 hidden h-full w-full md:grid">
+										<div class="full flex h-full items-center">
+											<p
+												class="${item.fixture.status.short !== 'FT'
+													? 'bg-lime-400 text-black'
+													: ' bg-red-200  text-red-500'} ml-[10%] flex w-[20%] items-center justify-center rounded-[10px] bg-red-200 px-[10px] py-[5px]
+                        text-red-500"
+											>
+												${item.fixture.status.short}
+											</p>
+											<p class="ml-[20%] w-[20%] text-yellow-400">
+												Time: ${moment(item.fixture.date).format('HH:MM')}
+											</p>
+											<p class="absolute right-[5%]">
+												<i
+													class="fa-solid fa-circle-info text-[25px] text-gray-200"
+												></i>
+											</p>
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
+							`
+						)}
 					</div>
 				</div>
 			</section>
