fe77f0b5848e279cb51b9c2aa7eddb2269209877
Donjete Tifeku <donjete.tifeku@life.gjirafa.com>
2023-02-05 20:52:21 +0100

    Fix Countries loader

--- src/loaders/allMatches-page/CountriesLoader.tsx
+++ src/loaders/allMatches-page/CountriesLoader.tsx
@@ -1,6 +1,10 @@
 export default function CountriesLoader() {
 	return (
 		<div>
+			<span className="flex h-12 items-center">
+				<span className="h-5 w-7 bg-slate-300 dark:bg-slate-700" />
+				<span className="ml-4 h-2 w-14 bg-slate-300 dark:bg-slate-700" />
+			</span>
 			{[...Array(20)].map((_, i) => (
 				<span
 					className="mb-4 flex animate-pulse items-center"
